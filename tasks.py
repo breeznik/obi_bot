56fbdd4f9795e863aec53dd7c6cdba98fe7156b0
Nikhil Rao <newraozxcode@gmail.com>
2025-08-23 16:51:59 +0530

    chore: production command

--- tasks.py
+++ tasks.py
@@ -2,6 +2,11 @@
 from invoke import task
 
 @task
-def run(c):
-    c.run("uvicorn app:app --reload")
+def dev(c):
+    """Run in development with auto-reload"""
+    c.run("uvicorn app:app --reload --host 0.0.0.0 --port 8000")
 
+@task
+def prod(c):
+    """Run in production mode"""
+    c.run("uvicorn app:app --host 0.0.0.0 --port 8000")
